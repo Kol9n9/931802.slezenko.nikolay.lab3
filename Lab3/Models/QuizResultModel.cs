cdfba1b60b72619c20550974452a8c564bd549a4
NikolayS <slezenkonikolay@mail.ru>
2022-03-27 20:15:55 +0700

    Done

---
+++ Lab3/Models/QuizResultModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab3.Models
+{
+    public class QuizResultModel
+    {
+        public List<QuizQuestionModel> Questions { get; set; }
+
+        public int CorrectAnswerCount
+        {
+            get
+            {
+                return Questions.Count(q => q.AnswerIsCorrect);
+            }
+        }
+
+        public int Count => Questions.Count;
+    }
+}
---
+++ Lab3/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab3
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { 
+                endpoints.MapControllerRoute("default","{controller=Mockups}/{action=Index}"); 
+            }); 
+        }
+    }
+}
